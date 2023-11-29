8da17ca5917630eafbcbd2d0fc3f5f80cd3910e0
Sean <34902361+seanzhang98@users.noreply.github.com>
2023-11-29 11:20:00 +0800

    Update SubscriptionListViewRow.swift

--- Sources/StoreHelper/Views/Shared/SubscriptionListViewRow.swift
+++ Sources/StoreHelper/Views/Shared/SubscriptionListViewRow.swift
@@ -35,7 +35,7 @@ public struct SubscriptionListViewRow: View {
     }
     
     public var body: some View {
-        Section() { Text(headerText).padding()}) {
+        Section() {
             // For each product in the group, display as a row using SubscriptionView().
             // If a product is the highest subscription level then pass its SubscriptionInfo to SubscriptionView().
             ForEach(products, id: \.id) { product in
